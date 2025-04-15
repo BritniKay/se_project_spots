4e7847930a827c4dc169673e88fb1caf87637f0f
BRITNI ELLIS <Britnikay09@gmail.com>
2025-04-15 11:03:21 -0400

    webpack: Add babel.config.js and node_modules

---
+++ src/pages/index.js
@@ -0,0 +1,147 @@
+import "../pages/index.css";
+import "../images/favicon.ico";
+import "../vendor/normalize.css";
+import {
+  enableValidation,
+  settings,
+  resetForm,
+  toggleButtonState,
+} from "../scripts/validation.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const profileName = document.querySelector(".profile__name-first");
+  const profileDescription = document.querySelector(
+    ".profile__description-first"
+  );
+  const inputProfileName = document.querySelector("#profile-name-input");
+  const inputProfileDescription = document.querySelector(
+    "#profile-description-input"
+  );
+  const profileEditButton = document.querySelector(".profile__edit-btn");
+  const profileEditModal = document.querySelector("#edit-modal");
+  const profileForm = document.forms["profile-form"];
+  const cardsContainer = document.querySelector(".cards__list");
+  const cardTemplate = document.querySelector("#card-template").content;
+  const addCardModal = document.querySelector("#add-card-modal");
+  const addCardForm = document.forms["add-card-form"];
+  const inputCardImageLink = document.querySelector("#profile-image-link");
+  const inputCardCaption = document.querySelector("#profile-caption-input");
+  const openAddCardModalButton = document.querySelector(".profile__add-btn");
+  const previewImageModal = document.querySelector("#preview-modal");
+  const previewImage = previewImageModal.querySelector(".modal__image");
+  const previewCaption = previewImageModal.querySelector(".modal__caption");
+  const closeModalButtons = document.querySelectorAll(".modal__close-btn");
+
+  function disableButton(buttonElement, config) {
+    buttonElement.classList.add(config.inactiveButtonClass);
+    buttonElement.disabled = true;
+  }
+
+  function renderCard(item, method = "prepend") {
+    if (!item.name || !item.link) return;
+    console.log("Rendering card:", item);
+    const cardElement = createCardElement(item);
+    method === "prepend"
+      ? cardsContainer.prepend(cardElement)
+      : cardsContainer.append(cardElement);
+  }
+
+  function createCardElement(data) {
+    console.log("Creating card element with data:", data);
+
+    const cardElement = cardTemplate.cloneNode(true).firstElementChild;
+    const cardTitle = cardElement.querySelector(".card__title");
+    const cardImage = cardElement.querySelector(".card__img");
+
+    cardTitle.textContent = data.name;
+    cardImage.src = require(`../images/${data.link}`);
+    cardImage.alt = data.name;
+
+    console.log("Image set to:", cardImage.src);
+
+    const deleteButton = cardElement.querySelector(".card__delete-button");
+    const likeButton = cardElement.querySelector(".card__like-button");
+
+    deleteButton.addEventListener("click", () => cardElement.remove());
+    likeButton.addEventListener("click", (event) =>
+      event.target.classList.toggle("liked")
+    );
+
+    cardImage.addEventListener("click", () => {
+      previewImage.src = require(`../images/${data.link}`);
+      previewImage.alt = data.name;
+      previewCaption.textContent = data.name;
+      openModal(previewImageModal);
+    });
+
+    return cardElement;
+  }
+
+  function openModal(modal) {
+    modal.classList.add("modal_opened");
+    document.addEventListener("keydown", handleEscClose);
+  }
+
+  function closeModal(modal) {
+    modal.classList.remove("modal_opened");
+    document.removeEventListener("keydown", handleEscClose);
+  }
+
+  function handleEscClose(event) {
+    if (event.key === "Escape") {
+      const openedModal = document.querySelector(".modal_opened");
+      if (openedModal) {
+        closeModal(openedModal);
+      }
+    }
+  }
+
+  enableValidation(settings);
+
+  profileEditButton.addEventListener("click", () => {
+    resetForm(profileForm, settings);
+    inputProfileName.value = profileName.textContent.trim();
+    inputProfileDescription.value = profileDescription.textContent.trim();
+    openModal(profileEditModal);
+  });
+
+  profileForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    profileName.textContent = inputProfileName.value.trim();
+    profileDescription.textContent = inputProfileDescription.value.trim();
+    closeModal(profileEditModal);
+  });
+
+  openAddCardModalButton.addEventListener("click", () =>
+    openModal(addCardModal)
+  );
+
+  addCardForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log("Form submitted");
+    const newCardData = {
+      name: inputCardCaption.value.trim(),
+      link: inputCardImageLink.value.trim(),
+    };
+    console.log("New card data:", newCardData);
+
+    if (!newCardData.name || !newCardData.link) return;
+    renderCard(newCardData);
+    addCardForm.reset();
+    disableButton(event.submitter, settings);
+    closeModal(addCardModal);
+  });
+
+  closeModalButtons.forEach((button) => {
+    const modal = button.closest(".modal");
+    button.addEventListener("click", () => closeModal(modal));
+  });
+
+  document.querySelectorAll(".modal").forEach((modal) => {
+    modal.addEventListener("mousedown", (event) => {
+      if (event.target === modal) {
+        closeModal(modal);
+      }
+    });
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: {
+    main: "./src/pages/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "",
+  },
+  mode: "development",
+  devtool: "inline-source-map",
+  stats: "errors-only",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 8080,
+    open: true,
+    liveReload: true,
+    hot: false,
+  },
+  target: ["web", "es5"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]",
+        },
+      },
+      {
+        test: /\.ico$/,
+        type: "asset/resource",
+        generator: {
+          filename: "icons/[name][ext]",
+        },
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: path.resolve(__dirname, "src/images/favicon.ico"),
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css", // Ensures CSS is extracted in `dist/`
+    }),
+  ],
+};
